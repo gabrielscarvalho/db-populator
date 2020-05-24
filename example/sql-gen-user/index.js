f44f93f90b1b59559234e3157c851af57e8fc475
Gabriel Santos Carvalho <gabrielsc@ciandt.com>
2020-05-24 18:53:58 -0300

    add full example

--- example/sql-gen-user/index.js
+++ example/sql-gen-user/index.js
@@ -2,17 +2,42 @@
 const insert = require('./insert');
 
 
-insert('t_customer', { 'email': 'john120@gmail.com' })
-const address1 = insert('t_address', { 'street': 'delivery address' });
+
+//First customer with 2 address
+// and 2 orders
+
+//you can access all the attributes of the customer.
+// the object you receive here, is simple: column: value.
+const customer = insert('t_customer', { 'email': 'john120@gmail.com' })
+
+//address has customer_id on it. you can add it typing:
+const address1 = insert('t_address', { 'street': 'delivery address', 'customer_id' : customer.id });
+
+//or you can get the default value, that in db-structure.js, is configured as:
+//   customer.id: { 'type': 'int', val: id.getNext('t_customer') },
+//   address.customer_id: { 'type': 'int', val: id.getCurrent('t_customer') },
+// at this mode, it will get the last inserted customer id:
 const address2 = insert('t_address', { 'street': 'invoice address' });
+
 insert('t_order', { 'delivery_ad': address1.id, 'invoice_ad': address2.id });
-insert('t_order_item', {})
 insert('t_order_item', {})
 
 insert('t_order', { 'delivery_ad': address1.id, 'invoice_ad': address2.id });
 insert('t_order_item', {})
-insert('t_order_item', {})
-insert('t_order_item', {})
+
+
+// Second customer with 1 address
+// 2 orders with 4 items
+insert('t_customer', { 'email': 'mary123@gmail.com' })
+insert('t_address', { 'street': 'delivery address' });
+insert('t_order', {});
+insert('t_order_item', {});
+insert('t_order_item', {});
+insert('t_order', {});
+insert('t_order_item', {});
+insert('t_order_item', {});
+insert('t_order_item', {});
+
 
 //insert.printData();
 insert.printSQLs();
--- example/sql-gen-user/insert.js
+++ example/sql-gen-user/insert.js
@@ -1,20 +1,35 @@
 const configure = require('db-populator/lib/insert');
 const id = require('db-populator/lib/id');
 const GenericSQLBuilder = require('db-populator/db/GenericSQLBuilder');
+const ValueStrategyParser = require('db-populator/lib/value-strategy-parser');
 
 const dbStructure = require('./db-structure');
 
 const initialIds = {
-    't_customer': 30,
-    't_address': 0,
-    't_order': 0,
-    't_order_item': 0
+    't_customer': 0,
+    't_address': 100,
+    't_order': 200,
+    't_order_item': 1500
 };
 
 
-id.setInitialIds(initialIds, (table, previousId) => {
+const valueStrategyParser = new ValueStrategyParser("'");// the unified quote symbol
+
+/* Uncomment the following code to see that you are able to create or replace types!
+valueStrategyParser.addParser('string', (val) =>  {
+  //format val as you wish
+  return valueStrategyParser.quoteChar + 'my string '+ val + valueStrategyParser.quoteChar;
+});
+*/
+
+
+id.setInitialIds(initialIds, (tableName, previousId) => {
     // you are able to define the rules to choose next id to each table.
-    return --previousId;
+    if(tableName == 't_customer') {
+        return --previousId;
+    }
+
+    return ++previousId;
 })
 
-module.exports = configure(new GenericSQLBuilder(dbStructure(id)));
+module.exports = configure(new GenericSQLBuilder(dbStructure(id), valueStrategyParser));
