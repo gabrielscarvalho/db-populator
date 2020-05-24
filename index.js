44b14f6ee49880be31c232d9c450e0c220ce189f
Gabriel Santos Carvalho <gabrielsc@ciandt.com>
2020-05-24 00:34:43 -0300

    example of customer

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const Random = require('./lib/random');
 
 
 const initialIds = {
-    't_order': 100,
-    't_consignment': 100,
+    't_customer': 1,
+    't_address': 32,
+    't_order': 100
 };
 
 const id = new ID(initialIds);
@@ -18,19 +19,41 @@ const valueStrategyParser = new ValueStrategyParser("'");
 
 
 const dbStructure = {
-    't_order': {
-        'id': { 'type': 'int', val: id.getNext('t_order'), column: 'order_id_comprido' },
-        'order_code': { type: 'string' , val: 'FILL_ORDER_CODE'},
-        'creation_date': { type: 'datetime' , val: Random.date({ addTime: true})},
-        'birth_date': { type: 'date' , val: Random.date({minYear: 1970, maxYear: 2010})}, 
-        'customer_name': { type: 'string', val: Random.fromList(['John', 'Paul', 'Suzan', 'Mark'])},
-        'total_value': { type: 'float', val: Random.number({min: 100, max: 200, decimals: 2})},
-        'age': { type: 'int', val: Random.number({min: 18, max: 50})},
+
+    't_customer': {
+        'id': { 'type': 'int', val: id.getNext('t_customer') },
+        'name': { type: 'string', val: Random.fromList(['John', 'Paul', 'Suzan', 'Mark']) },
+        'surname': { type: 'string', val: Random.fromList(['Doe', 'Mcknight', 'Gates', 'Jobs']) },
+        'email': { type: 'string', val: Random.fromDoubleList(['John', 'Paul', 'Suzan', 'Mark'], ['@gmail.com', '@hotmail.com']) },
+        'birthDate': { type: 'date', val: Random.date({ minYear: 1970, maxYear: 2010 }) },
+        'creation_date': { type: 'datetime', val: Random.date({ addTime: true, minYear: 2018, maxYear: 2022 }) }
     },
-    't_consignment': {
-        'id': { 'type': 'int', val: id.getNext('t_consignment')},
-        'order_id': { 'type': 'int', val: id.getCurrent('t_order')},
-        'order_code': { 'type': 'string', val: 'FILL_ORDER_CODE'},
+    't_address': {
+        'id': { 'type': 'int', val: id.getNext('t_address') },
+        'customer_id': { 'type': 'int', val: id.getCurrent('t_customer') },
+        'street': { type: 'string', val: Random.fromList(['St. Abc', 'St. Cde']) },
+        'number': { type: 'int', val: Random.number({ min: 1, max: 150 }) },
+        'country': { type: 'string', val: Random.fromList(['Brazil', 'United States']) },
+        'creation_date': { type: 'datetime', val: Random.date({ addTime: true, minYear: 2018, maxYear: 2022 }) },
+        'is_main_address': { type: 'bool', val: Random.fromList([true, false]) }
+    },
+    't_order': {
+        'id': { 'type': 'int', val: id.getNext('t_order') },
+        'customer_id': { 'type': 'int', val: id.getCurrent('t_customer') },
+        'delivery_ad': { 'type': 'int', val: id.getCurrent('t_address'), column: 'delivery_address_id' },
+        'invoice_ad': { 'type': 'int', val: id.getCurrent('t_address'), column: 'invoice_address_id' },
+        'total_price': { 'type': 'float', val: Random.number({ min: 200, max: 500, decimals: 2 }) },
+        'creation_date': { type: 'datetime', val: Random.date({ addTime: true, minYear: 2018, maxYear: 2022 }) },
+        'status': { type: 'string', val: 'PROCESSING' }
+    },
+    't_order_item': {
+        'id': { 'type': 'int', val: id.getNext('t_order_item') },
+        'order_id': { 'type': 'int', val: id.getCurrent('t_order') },
+        'product_name': { type: 'string', val: Random.fromList(['Iphone 11', 'Samsung VT 42', 'Notebook LG']) },
+        'total_price': { 'type': 'float', val: Random.number({ min: 200, max: 500, decimals: 2 }) },
+        'discount_price': { 'type': 'float', val: Random.number({ min: 10, max: 50, decimals: 2 }) },
+        'qty': { 'type': 'int', val: Random.number({ min: 1, max: 3 }) },
+        
     }
 };
 
@@ -38,14 +61,17 @@ const dbStructure = {
 
 const insert = configure(new PostgreSQL(dbStructure, valueStrategyParser));
 
-const order1 = insert('t_order', {'order_code' : 'NEW_ORDER_CODE'})
-insert('t_consignment', { 'order_code' : order1.order_code})
-insert('t_consignment', 'blablabal')
-const order2 = insert('t_order', {'order_code' : 'NEW_ORDER_CODE_2'})
-insert('t_consignment', { 'order_code' : order2.order_code});
+insert('t_customer', { 'email': 'john120@gmail.com' })
+const address1 = insert('t_address', { 'street': 'delivery address' });
+const address2 = insert('t_address', { 'street': 'invoice address' });
+insert('t_order', { 'delivery_ad': address1.id, 'invoice_ad': address2.id });
+insert('t_order_item',{})
+insert('t_order_item',{})
 
-insert('t_order', {'order_code' : 'NEW_ORDER_CODE3'})
-insert('t_order', {'order_code' : 'NEW_ORDER_CODE4'})
-insert('t_order', {'order_code' : 'NEW_ORDER_CODE5'})
+insert('t_order', { 'delivery_ad': address1.id, 'invoice_ad': address2.id });
+insert('t_order_item',{})
+insert('t_order_item',{})
+insert('t_order_item',{})
 
+//insert.printData();
 insert.printAll();
