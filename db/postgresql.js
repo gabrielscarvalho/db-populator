5abb4738285932649d3e3038d84ef8b09aaef7a2
Gabriel Santos Carvalho <gabrielsc@ciandt.com>
2020-05-23 22:48:49 -0300

    still working

--- db/postgresql.js
+++ db/postgresql.js
@@ -1,18 +1,31 @@
 
-
-
-
-
-
 class PostgreSQL {
 
     constructor(dbStructure) {
-        this.dbStructure = dbStructure;
+        this.structure = dbStructure;
+        this.sqls = [];
     }
 
 
-    insert(table, manualChanges) {
-            
+    insert(tableName, dataRow) {
+        let sql = 'INSERT INTO ' + tableName;
+
+        let columnNames = [];
+        let values = [];
+
+        for(const columnName in dataRow) {
+            const column = dataRow[columnName];
+            columnNames.push(columnName);
+            values.push(column.val);
+        }
+
+
+        sql  = sql + "(" + columnNames.join(',') + ") ";
+
+        sql  = sql + " VALUES (" + values.join(',') + ");";
+
+        console.log(sql);
+        this.sqls.push(sql);
     }
 
 }
--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 const configure = require('./lib/insert');
-const id = require('./lib/id');
+const ID = require('./lib/id');
+
 const PostgreSQL = require('./db/postgresql');
 
 
 
 const initialIds = {
-    't_order': 100
+    't_order': 100,
+    't_consignment': 100,
 };
 
-const ID = new id(initialIds);
+const id = new ID(initialIds);
 
 
 
 const dbStructure = {
     't_order': {
-        'id' : ID.getNext('t_order')
+        'id': { 'type': 'int', val: id.getNext('t_order'), column: 'order_id_comprido' },
+        'order_code': { type: 'string' , val: 'FILL_ORDER_CODE'}
+    },
+    't_consignment': {
+        'id': { 'type': 'int', val: id.getNext('t_consignment')},
+        'order_id': { 'type': 'int', val: id.getCurrent('t_order')},
+        'order_code': { 'type': 'string', val: 'FILL_ORDER_CODE'},
     }
-}
+};
 
 
 
 const insert = configure(new PostgreSQL(dbStructure));
 
-insert('t_order', 'blablabal')
+const order1 = insert('t_order', {'order_code' : 'NEW_ORDER_CODE'})
+insert('t_consignment', { 'order_code' : order1.order_code})
+insert('t_consignment', 'blablabal')
+const order2 = insert('t_order', {'order_code' : 'NEW_ORDER_CODE_2'})
+insert('t_consignment', { 'order_code' : order2.order_code})
