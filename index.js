cefcae80738ea0bb2ad13e8237d216bb4e7985b2
Gabriel Santos Carvalho <gabrielsc@ciandt.com>
2020-05-24 00:01:45 -0300

    its parsing date now

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const configure = require('./lib/insert');
 const ID = require('./lib/id');
 const ValueStrategyParser = require('./lib/value-strategy-parser');
 const PostgreSQL = require('./db/postgresql');
+const Random = require('./lib/random');
 
 
 
@@ -16,18 +17,15 @@ const valueStrategyParser = new ValueStrategyParser("'");
 
 
 
-valueStrategyParser.addParser('datetime', (val) => {
-
-    return '"DATETIME + ' + val + '"'
-});
-
-
-
 const dbStructure = {
     't_order': {
         'id': { 'type': 'int', val: id.getNext('t_order'), column: 'order_id_comprido' },
         'order_code': { type: 'string' , val: 'FILL_ORDER_CODE'},
-        'creation_date': { type: 'datetime' , val: 'FILL_ORDER_CODE'}
+        'creation_date': { type: 'datetime' , val: Random.date({ addTime: true})},
+        'birth_date': { type: 'date' , val: Random.date({minYear: 1970, maxYear: 2010})}, 
+        'customer_name': { type: 'string', val: Random.fromList(['John', 'Paul', 'Suzan', 'Mark'])},
+        'total_value': { type: 'float', val: Random.number({min: 100, max: 200, decimals: 2})},
+        'age': { type: 'int', val: Random.number({min: 18, max: 50})},
     },
     't_consignment': {
         'id': { 'type': 'int', val: id.getNext('t_consignment')},
@@ -46,5 +44,8 @@ insert('t_consignment', 'blablabal')
 const order2 = insert('t_order', {'order_code' : 'NEW_ORDER_CODE_2'})
 insert('t_consignment', { 'order_code' : order2.order_code});
 
+insert('t_order', {'order_code' : 'NEW_ORDER_CODE3'})
+insert('t_order', {'order_code' : 'NEW_ORDER_CODE4'})
+insert('t_order', {'order_code' : 'NEW_ORDER_CODE5'})
 
 insert.printAll();
