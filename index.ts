6ffa8a2c6d40000dce134c8fc799e7f26ce5059f
Gabriel Santos Carvalho <gabrielsc@ciandt.com>
2020-06-05 22:36:09 -0300

    added random

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import Id from './src/database/value/value-generator/id';
 import Random from './src/database/value/value-generator/random';
 import DataRow from './src/data/DataRow';
 
+
 const id = new Id();
 
 const db: Database = new Database();
@@ -15,14 +16,14 @@ const db: Database = new Database();
 const customer: Table = db.newTable('t_customer')
     .addColumn('id', 'int', id.getNext('t_customer.id'), 'customer_id')
     .addColumn('name', 'string', Random.name())
-    .addColumn('email', 'string', Random.number())
+    .addColumn('email', 'string', Random.email())
     .addPrimaryKey('id');
 
 
 const address: Table = db.newTable('t_address')
     .addColumn('id', 'int', id.getNext('t_address.id'), 'address_id')
     .addColumnReference('customerId', 'int', customer.getColumn('id'), 'customer_id')
-    .addColumn('street', 'string', Random.name())
+    .addColumn('street', 'string', Random.string('str.'))
     .addPrimaryKey('id');
 
 
@@ -30,7 +31,7 @@ const order: Table = db.newTable('t_order')
     .addColumn('id', 'int', id.getNext('t_order.id'), 'order_id')
     .addColumnReference('customerId', 'int', customer.getColumn('id'), 'customer_id')
     .addColumnReference('deliveryId', 'int', address.getColumn('id'), 'delivery_id')
-    .addColumn('price', 'int', Random.number())
+    .addColumn('price', 'int', Random.number({ min: 10, max: 200, decimals: 2}))
     .addPrimaryKey('id');
 
 
@@ -39,7 +40,7 @@ const consign: Table = db.newTable('t_consignment');
 
 consign.addColumn('id', 'int', id.getNext('t_consignment.id'), 'consignment_id')
     .addColumnReference('orderId', 'int', order.getColumn('id'),'order_id')
-    .addColumn('price', 'int', Random.number(),'order_total_price')
+    .addColumn('price', 'int', Random.number({ min: 10, max: 200, decimals: 2}),'order_total_price')
     .addPrimaryKey('id')
     .addPrimaryKey('orderId');
 
